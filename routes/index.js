74ed4ef24c741c9da80c0fdf6f7fe7d481f5285d
ITDragon <1625402590@qq.com>
2016-06-18 17:37:13 +0800

    发表博客

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 
 module.exports = function(app){
 	app.get('/',function(req,res){
@@ -10,6 +11,7 @@ module.exports = function(app){
 			error:req.flash('error').toString()
 		});
 	});
+	app.get('/reg', checkNotLogin);
 	app.get('/reg',function(req,res){
 		res.render('reg',{
 			title:'注册',
@@ -18,6 +20,7 @@ module.exports = function(app){
 			error:req.flash('error').toString()
 		});
 	});
+	app.get('/reg', checkNotLogin);
 	app.post('/reg',function(req,res){
 		var name = req.body.name,
 		password = req.body.password,
@@ -58,6 +61,7 @@ module.exports = function(app){
 			});
 		});
 	});
+	app.get('/login', checkNotLogin);
 	app.get('/login',function(req,res){
 		res.render('login',{
 			title:'登录',
@@ -66,6 +70,7 @@ module.exports = function(app){
 			error:req.flash('error').toString()
 		});
 	});
+	app.post('/login', checkNotLogin);
 	app.post('/login',function(req,res){
 		// 生成密码的md5值
 		var md5 = crypto.createHash('md5');
@@ -87,14 +92,35 @@ module.exports = function(app){
 			res.redirect('/');
 		});
 	});
+	app.get('/post', checkLogin);
 	app.get('/post',function(req,res){
 		res.render('post',{title:'发表'});
 	});
+	app.get('/post', checkLogin);
 	app.post('/post',function(req,res){
+		var currentUser = req.session.user,
+		var post = new Post();
 	});
+	app.get('/logout', checkLogin);
 	app.get('/logout',function(req,res){
 		req.session.user = null;
 		req.flash('success', '登出成功!');
 		res.redirect('/login');
 	});
+	
+	function checkLogin(req,res,next){
+		if(!req.session.user){
+			req.flash('error','用户未登录');
+			res.redirect('/login');
+		}
+		next();
+	}
+	
+	function checkNotLogin(req,res,next){
+		if(req.session.user){
+			req.flash('error','已登录');
+			res.redirect('back');
+		}
+		next();
+	}
 };
